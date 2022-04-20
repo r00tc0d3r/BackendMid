648722397d887813c1ae4ea320c45b2a952da9e2
r00tc0d3r <wowvaltalas123@gmail.com>
2022-04-20 11:03:43 -0300

    20-04 Class

--- ProyectoEjemplo/index.js
+++ ProyectoEjemplo/index.js
@@ -1,3 +1,16 @@
+const http = require("http");
 const { env, port } = require("./config");
 
-console.log(env, port);
+const server = http.createServer();
+server.on("request", (request, response) => {
+  if (request.method === "POST" && request.url === "/datos") {
+    // TODO: Regresar al momento de ver streams
+  }
+
+  response.statusCode = "200";
+  response.end("Hola Mundo");
+});
+
+server.listen(port);
+console.log("Servidor funcionando en http://localhost:" + port);
+console.log(env);
